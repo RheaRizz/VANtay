ba68330a02acf42ed62f58dce070e54aa26188ce
LoisAlonsagayGit <loisalonsagay@gmail.com>
2024-05-13 20:21:55 +0800

    Front-end updated login, signup, cashier page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/pages/LoginPage';
 import SignupPage from './components/pages/SignupPage';
+import HomePage from './components/pages/HomePage';
+import AdminPage from './components/pages/AdminPage'; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminPage />} /> 
       </Routes>
     </Router>
   );
